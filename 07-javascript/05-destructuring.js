4c778fc22cf193f50f2c3060e0162de76a2e829a
turrilld <turrilld@pdx.edu>
2024-05-31 12:36:35 -0700

    Lab 7 part 5

--- 07-javascript/05-destructuring.js
+++ 07-javascript/05-destructuring.js
@@ -1,35 +1,44 @@
 const destructureItems = (input) => {
   // destructure the name and parents of the character and format the output as shown
+  const { first, last } = input;
+  const { mother, father } = input.allegiance.parents;
 
-  return input;
+  // Sansa Stark
+  //   daughter of
+  //   Catelyn Tully and Eddard Stark
+  const result = `${first} ${last}
+    daughter of
+    ${mother} and ${father}`;
+
+  return result;
 };
 
 const sansa = {
-  first: 'Sansa',
-  last: 'Stark',
+  first: "Sansa",
+  last: "Stark",
   allegiance: {
-    house: 'Winterfell',
+    house: "Winterfell",
     parents: {
-      mother: 'Catelyn Tully',
-      father: 'Eddard Stark',
+      mother: "Catelyn Tully",
+      father: "Eddard Stark",
     },
     animals: {
-      direwolf: 'Lady',
+      direwolf: "Lady",
     },
   },
 };
 
 const daenerys = {
-  first: 'Daenerys',
-  last: 'Targaryen',
+  first: "Daenerys",
+  last: "Targaryen",
   allegiance: {
-    house: 'Targaryen',
+    house: "Targaryen",
     parents: {
-      mother: 'Queen Rhaella',
-      father: 'King Aerys II Targaryen',
+      mother: "Queen Rhaella",
+      father: "King Aerys II Targaryen",
     },
     animals: {
-      dragons: 'Drogon',
+      dragons: "Drogon",
     },
   },
 };
